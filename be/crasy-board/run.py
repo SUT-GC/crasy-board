d2f147e92e9818f87e9820d59dfed7240a22427f
gc <gc@gcs-MacBook-Pro.local>
2018-05-16 20:36:32 +0800

    增加login和index的相互跳转

--- be/crasy-board/run.py
+++ be/crasy-board/run.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from api.login import (
+    login_page,
+)
+
+from api.index import (
+    index_page
+)
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'hello world'
+app.register_blueprint(login_page, url_prefix='/login')
+app.register_blueprint(index_page, url_prefix='/index')
 
 if __name__ == "__main__":
     app.run()
--- fe/crasy-board/src/pages/index/Index.vue
+++ fe/crasy-board/src/pages/index/Index.vue
@@ -9,6 +9,17 @@
 export default {
   name: 'Index',
   components: {
+  },
+  created: function() {
+    this.$http.get('http://localhost:5000/login/').then(function(response) {
+        let data = response.data
+        console.log(data)
+        if (data || data.code !== 200) {
+          window.location.href="/login.html"
+        }
+    }, function(response) {
+
+    })
   }
 }
 </script>
