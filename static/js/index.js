31b696a25a61ba8a65c79758f048c3ff96244c26
SUT-GC <gouchao2008@qq.com>
2018-05-20 16:49:54 +0800

    增加登陆成功跳转

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,55 @@
 var app = new Vue({
-    el: "#app",
+    el: "#index",
     data: {
         message: "hello world",
-        chartData: {
-            columns: ['日期', '销售额'],
+        cpuData: {
+            columns: ['类型', '大小'],
             rows: [
-                { '日期': '1月1日', '销售额': 123 },
-                { '日期': '1月2日', '销售额': 1223 },
-                { '日期': '1月3日', '销售额': 2123 },
-                { '日期': '1月4日', '销售额': 4123 },
-                { '日期': '1月5日', '销售额': 3123 },
-                { '日期': '1月6日', '销售额': 7123 }
+                { '类型': '空闲中', '大小': 1523},
+                { '类型': '使用中', '大小': 1223},
             ]
-        }
+        },
+        cpuChartSettings: {
+            dimension: '类型',
+            radius: 120,
+            offsetY: 220,
+        },
+        ramData: {
+            columns: ['类型', '大小'],
+            rows: [
+                { '类型': '空闲中', '大小': 1523},
+                { '类型': '使用中', '大小': 1223},
+            ]
+        },
+        ramChartSettings: {
+            dimension: '类型',
+            radius: 120,
+            offsetY: 220,
+        },
+        storageData: {
+            columns: ['日期', '销售额-1季度', '销售额-2季度', '占比', '其他'],
+            rows: [
+                { '日期': '1月1日', '销售额-1季度': 1523, '销售额-2季度': 1523, '占比': 0.12, '其他': 100 },
+                { '日期': '1月2日', '销售额-1季度': 1223, '销售额-2季度': 1523, '占比': 0.345, '其他': 100 },
+                { '日期': '1月3日', '销售额-1季度': 2123, '销售额-2季度': 1523, '占比': 0.7, '其他': 100 },
+                { '日期': '1月4日', '销售额-1季度': 4123, '销售额-2季度': 1523, '占比': 0.31, '其他': 100 },
+                { '日期': '1月5日', '销售额-1季度': 3123, '销售额-2季度': 1523, '占比': 0.12, '其他': 100 },
+                { '日期': '1月6日', '销售额-1季度': 7123, '销售额-2季度': 1523, '占比': 0.65, '其他': 100 }
+            ]
+        },
+        storageLineSettings:{},
+        netData: {
+            columns: ['日期', '销售额-1季度', '销售额-2季度', '占比', '其他'],
+            rows: [
+                { '日期': '1月1日', '销售额-1季度': 1523, '销售额-2季度': 1523, '占比': 0.12, '其他': 100 },
+                { '日期': '1月2日', '销售额-1季度': 1223, '销售额-2季度': 1523, '占比': 0.345, '其他': 100 },
+                { '日期': '1月3日', '销售额-1季度': 2123, '销售额-2季度': 1523, '占比': 0.7, '其他': 100 },
+                { '日期': '1月4日', '销售额-1季度': 4123, '销售额-2季度': 1523, '占比': 0.31, '其他': 100 },
+                { '日期': '1月5日', '销售额-1季度': 3123, '销售额-2季度': 1523, '占比': 0.12, '其他': 100 },
+                { '日期': '1月6日', '销售额-1季度': 7123, '销售额-2季度': 1523, '占比': 0.65, '其他': 100 }
+            ]
+        },
+        netLineSettings:{}
     },
     methods: {
         handleOpen(key, keyPath) {
@@ -20,6 +57,21 @@ var app = new Vue({
         },
         handleClose(key, keyPath) {
             console.log(key, keyPath);
+        },
+        logout(){
+            username = getCookie(document, "crasy-board-user")
+            console.log('logout', username)
+
+            this.$http.post("/logout/", JSON.stringify({"username": username})).then(function(response){
+                let loginResult = response.data
+                console.log(loginResult)
+                if(loginResult && loginResult.code === 200) {
+                    setCookie(document, 'crasy-board-user', loginResult.data)
+                    window.location.href="/"
+                }
+            }, function(response){
+
+            });
         }
     }
 })
